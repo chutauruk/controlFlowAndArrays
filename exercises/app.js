653cafb4b91f33378188fb9753da34e0e2522237
Cynthia Hutauruk <cyn.uruk@gmail.com>
2022-10-12 14:55:42 -0600

    second commit

--- exercises/app.js
+++ exercises/app.js
@@ -1,1 +1,29 @@
 console.log("hello world")
+
+function testNum(num) {
+    if (num < 0);
+        console.log("This is a negative integer");
+    }
+    else if (num >= 0) {
+        console.log("This is a positive integer.");
+    }
+
+    testNum(5);
+    testNum(-7)
+
+    function testAdult(age) {
+        if (age < 18);
+            console.log("Access Denied.");
+        }
+        else if (num > 18) {
+            console.log("Access Granted.");
+        }
+        else if (num == 18) {
+            console.log("Access Granted.")
+        }
+        else
+            console.log("Please enter your age again.")
+
+        testAge(19);
+        testAge(16)
+        testAge(Dog)
