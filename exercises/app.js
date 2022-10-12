69fd7dad6cb5b487324bcda584f5a71607f5de8e
Cynthia Hutauruk <cyn.uruk@gmail.com>
2022-10-12 15:55:31 -0600

    letter grade exercise

--- exercises/app.js
+++ exercises/app.js
@@ -4,7 +4,7 @@ function testNum(num) {
     if (num < 0);
         console.log("This is a negative integer");
     }
-    else if (num >= 0) {
+    else (num >= 0) {
         console.log("This is a positive integer.");
     }
 
@@ -15,10 +15,10 @@ function testNum(num) {
         if (age < 18);
             console.log("Access Denied.");
         }
-        else if (num > 18) {
+        else (num > 18) {
             console.log("Access Granted.");
         }
-        else if (num == 18) {
+        else (num == 18) {
             console.log("Access Granted.")
         }
         else
@@ -27,3 +27,33 @@ function testNum(num) {
         testAge(19);
         testAge(16)
         testAge(Dog)
+
+function var(num) {
+    if (num > 100);
+        console.log("Positive number");
+    }
+    else (num < 100); {
+        console.log("Positive number");
+    }
+    else (num < 0); {
+        console.log("Negative number");
+    }
+
+
+function letter(grade) {
+    if (grade >= 90);
+        console.log("A");
+    }
+    else if (grade >= 80 && < 90) {
+        console.log("B");
+    }   
+    else if (grade >= 70 && < 80) {
+        console.log("C");
+    }
+    else if (grade >= 55 && < 70) {
+        console.log("D");
+    }
+    else (grade < 55) {
+        console.log("F")
+    }
+   
