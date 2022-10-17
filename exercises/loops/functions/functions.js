a447834684d4c894a686aab7523fbc98f14554ce
Cynthia Hutauruk <cyn.uruk@gmail.com>
2022-10-17 14:42:12 -0600

    objects and classes

--- exercises/loops/functions/functions.js
+++ exercises/loops/functions/functions.js
@@ -1,5 +1,5 @@
 //function declaration
-function one() {
+/* function one() {
         return 'one'
 }
 one(); //function expression
@@ -7,3 +7,16 @@ const two = () =>{
     return 2;
 }
 const shortenedTwo = () => 2;
+
+function areBothEven(n1,n2) {
+    return !(n1 % 2) && !(n2 % 2);
+}
+
+console.log(areBothEven(3,7)); */
+
+const areaOfTriangle (base, height) => {
+    areaOfTriangle = base * height/2
+    console.log('The area of a triangle with a width of ${base} and length of ${height} is ${areaOfTriangle} square units.')
+}
+
+areaOfTriangle(5,2);
